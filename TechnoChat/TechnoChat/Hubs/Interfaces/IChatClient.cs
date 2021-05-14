d3e3b0a8cb9c22a36ed716c97e12a8ebfa1c9d25
Person M <michael.person@cognitic.be>
2021-05-14 15:28:17 +0200

    Add two interfaces for server hub and client

---
+++ TechnoChat/TechnoChat/Hubs/Interfaces/IChatClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechnoChat.Hubs.Interfaces
+{
+    /// <summary>
+    /// Interface conditionnant le côté client de SignalR
+    /// </summary>
+    public interface IChatClient
+    {
+        /// <summary>
+        /// Permet de recevoir un message individuel ou de groupe
+        /// </summary>
+        /// <param name="message">Le message envoyé</param>
+        /// <returns>La task</returns>
+        Task ReceiveMessage(string message,string user, string groupName="");
+        /// <summary>
+        /// Permet de recevoir le changement de status d'un utilisateur
+        /// </summary>
+        /// <param name="status">le status de l'utilisateur <seealso cref="EStatus"/></param>
+        /// <param name="user">L'utilisateur ayant changé de status</param>
+        /// <returns>La Task</returns>
+        Task StatusChanged(EStatus status, string user);
+        /// <summary>
+        /// Permet de savoir lorsqu'un utilisateur rejoint un groupe
+        /// </summary>
+        /// <param name="groupName">Le nom du groupe</param>
+        /// <param name="user">L'utilisateur</param>
+        /// <returns>La Task</returns>
+        Task MemberHasJoinGroup(string groupName, string user);
+        /// <summary>
+        /// Permet de savoir lorsqu'un utilisateur quitte un groupe
+        /// </summary>
+        /// <param name="groupName">Le nom du groupe</param>
+        /// <param name="user">L'utilisateur</param>
+        /// <returns>La Task</returns>
+        Task MemberHasLeaveGroup(string groupName, string user);
+        /// <summary>
+        /// Permet d'attire l'attention d'un utilisateur
+        /// </summary>
+        /// <param name="user">L'utilisateur ayant lancé le wizz</param>
+        /// <returns>La Task</returns>
+        Task Wizz(string user);
+    }
+}
---
+++ TechnoChat/TechnoChat/Hubs/Interfaces/IChatHub.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechnoChat.Hubs.Interfaces
+{
+    /// <summary>
+    /// Enumération des status disponibles
+    /// </summary>
+    public enum EStatus { OnLine, OffLine, DoNotDisturb }
+    /// <summary>
+    /// Interface conditionnant le côté serveur de SignalR
+    /// </summary>
+    public interface IChatHub
+    {
+        /// <summary>
+        /// Permet d'envoyer un message à tous les users ou à un user particulier ou à un groupe
+        /// </summary>
+        /// <param name="message">Le message a transmettre</param>
+        /// <param name="recipient">L'utilisateur spécifique ( par défaut : Tous)</param>
+        /// <param name="groupName">Le groupe spécifique ( par défaut : Aucun)</param>
+        /// <returns>La Task</returns>
+        Task SendMessage(string message, string recipient="Tous", string groupName="Aucun");
+        /// <summary>
+        /// Permet de demander son changement de status
+        /// </summary>
+        /// <param name="status">le status de l'utilisateur <seealso cref="EStatus"/></param>
+        /// <returns>La Task</returns>
+        Task ChangeStatus(EStatus status);
+        /// <summary>
+        /// Permet de rejoindre un groupe
+        /// </summary>
+        /// <param name="groupName">Le nom du groupe</param>
+        /// <returns>La Task</returns>
+        Task MemberJoinGroup(string groupName);
+        /// <summary>
+        /// Permet de quitter un groupe
+        /// </summary>
+        /// <param name="groupName">Le nom du groupe</param>
+        /// <returns>La Task</returns>
+        Task MemberLeaveGroup(string groupName);
+        /// <summary>
+        /// Permet d'attirer l'attention d'un utilisateur ou d'un groupe
+        /// </summary>
+        /// <param name="recipient">L'utilisateur spécifique ( par défaut : Tous)</param>
+        /// <param name="groupName">Le groupe spécifique ( par défaut : Aucun)</param>
+        /// <returns>La Task</returns>
+        Task Wizz(string recipient = "Tous", string groupName = "Aucun");
+
+    }
+}
